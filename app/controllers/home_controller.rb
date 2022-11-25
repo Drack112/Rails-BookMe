876f3b129e77a19da3e8320b96ebf8208f83dc62
Drack112 <onepessoa3@gmail.com>
2022-11-25 17:47:30 -0300

    ⚡ General Update:  Dashboard Route

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
+
+  before_action :authenticate_user!, only: [:dasboard]
+
   def index
+  end
+
+  def dashboard
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  authenticated :user do
+    root to: 'home#dashboard', as: :authenticated_root
+  end
+
   root to: 'home#index'
 end
